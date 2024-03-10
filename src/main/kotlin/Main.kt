b59899b56d9648ef1c7790bf86e1771aaf796062
vlad hrabvchak <bigfutnk@gmail.com>
2024-03-10 16:45:27 +0100

    v2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,42 +1,72 @@
 fun main(args: Array<String>) {
-    val hh: Array<String> = arrayOf("5","4","1","3", "/", "3", "3","-","33")
-    //"-", "11")
-    //"+", "22", "33")
-    var aa = false
-    var ll = 1
-    var num1 = ""
-    var num2 = ""
-    var sigh = ""
-    var res = 0
-   for(gg in hh) {
-       if (gg == "+" || gg == "-" || gg == "*" || gg == "/") {
-           ll = ll + 1
-           if(aa == false){
-           sigh = gg
+    val hh: Array<String> = arrayOf("1", "3", "2", "4", "+", "4", "-", "3", "3", "*", "5", "8", "0", "-", "4", "5", "/", "4")
+    val jj = mutableListOf<String>()
+    val pp = mutableListOf<String>()
+    val oo = mutableListOf<String>()
+    var sigh1 = ""
+    var l = ""
+    var num1 = 0
+    var nn = 0
+
+    var num2 = 0
+    pp.add("")
+    for (gg in hh) {
+        if (gg == "+" || gg == "-" || gg == "*" || gg == "/") {
+            pp.add(gg)
+            jj.add("|")
+        } else {
+            jj.add(gg)
+        }
+
+
+    }
+    jj.add("|")
+    for (kk in jj) {
+        if (kk != "|") {
+            l = l + kk
+        } else {
+            oo.add(l)
+            l = ""
+        }
+    }
+    for (aa in oo.indices){
+
+        // if(aa % 2 == 0){
+           num2 = oo[aa].toInt()
+           sigh1 = pp[aa]
+           when(sigh1){
+               "+" -> num1 = num1 + num2
+               "-" -> num1 = num1 - num2
+               "*" -> num1 = num1 * num2
+               "/" -> num1 = num1 / num2
+               "" -> num1 = num1 + num2
            }
-           aa = !aa
-       }else {
-           if (ll == 1) {
-               num1 = num1 + gg
-           }else if(ll == 2){
-               num2 = num2 + gg
-           }else if(ll == 3){
-               when(sigh){
-                   "+" -> res = num1.toInt() + num2.toInt()
-                   "-" -> res = num1.toInt() - num2.toInt()
-                   "*" -> res = num1.toInt() * num2.toInt()
-                   "/" -> res = num1.toInt() / num2.toInt()
-               }
-           }
 
-       }
+           num2 = 0
+        nn = nn + 1
 
-       }
+    }
 
 
 
-           println(num1)
-           println(num2)
-           println(res)
+}
 
-       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
